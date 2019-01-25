0f28bddcab98a0f1356739e4e253a567c15ca9ea
Pierre TAQUET <pierre.taquet@gmail.com>
2019-01-25 09:20:23 +0100

    Initial application generated by JHipster-5.7.2

---
+++ src/main/java/fr/olympp/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package fr.olympp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Backend 2.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
---
+++ src/main/java/fr/olympp/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package fr.olympp.web.rest.vm;
